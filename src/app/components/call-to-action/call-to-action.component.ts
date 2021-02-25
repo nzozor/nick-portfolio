d59b934c9884a85124c2b6828dc80ec5774de983
nick.z <nzozor@gmail.com>
2021-02-25 22:01:28 +0000

    fix call to action fix contact margin

--- src/app/components/call-to-action/call-to-action.component.ts
+++ src/app/components/call-to-action/call-to-action.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallToActionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
+  scrollTop(): void {
+    const id = 'scroll-top-anchor';
+    this.viewportScroller.scrollToAnchor(id);
+  }
 }
--- src/app/shared/modules/call-to-action/call-to-action.module.ts
+++ src/app/shared/modules/call-to-action/call-to-action.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CallToActionComponent } from 'src/app/components/call-to-action/call-to-action.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [CallToActionComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [CallToActionComponent]
 })
