9744f63ecb0ba7a68220436af973d7d3111fdadc
nick.z <nick.zozor@onefile.co.uk>
2021-05-20 20:59:43 +0100

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   mockdata: false,
   endpoints: {
     express: {
-      url: `http://localhost:3000/api/v1`,
+      url: `http://167.99.200.71:3000/api/v1`,
       version: 'v1'
     },
   }
