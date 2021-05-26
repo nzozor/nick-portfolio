c2a4208a18c26b60300a21a626b2b7ff8748ee8e
nick.z <nick.zozor@onefile.co.uk>
2021-05-26 17:30:47 +0100

    enable http on client and api server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  mockdata: true,
+  mockdata: false,
   endpoints: {
     express: {
-      url: `api`,
-      // url: `https://167.99.200.71:3000/api/v1`,
+      // url: `api`,
+      url: `https://cms.elsabenoldi.com/api/v1`,
       version: 'v1'
     },
   }
